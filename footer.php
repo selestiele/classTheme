659c7cd0298e933585445623990b9ae97f609ca1
Liz <admin@elizabethmbank.info>
2018-03-27 19:32:52 -0400

    added nav menu in footer. adjusted stylesheet.

--- footer.php
+++ footer.php
@@ -1,4 +1,13 @@
 <?php  ?>
-
+<footer>
+    <?php wp_nav_menu( array(
+         'menu' => 'footer',
+         'container' => 'nav',
+         'container_class' => 'footer-nav',
+         'before' => '',
+    )); ?>
+    
+    <p>Copyright <?php echo date('Y') ?> Elizabeth Bank</p>
+</footer>
 </body>
 </html>
--- functions.php
+++ functions.php
@@ -4,7 +4,7 @@ function liztheme_styles() {
     //link the stylesheet
     wp_register_style('style', get_template_directory_uri() . '/style.css', array(), '0.1');
     wp_register_style('google-fonts', 'https://fonts.googleapis.com/css?family=Lato|Raleway');
-    wp_register_style('fa-fonts', get_template_directory_uri() . '/fontAwesome/webfonts/');
+    wp_register_style('fontAwesome', get_template_directory_uri() . '/fontAwesome/css/fontawesome-all.css');
     
     //enqueue the stylesheets
     wp_enqueue_style('style');
@@ -15,7 +15,8 @@ add_action('wp_enqueue_scripts', 'liztheme_styles');
 
 register_nav_menus( array(
      'main' => __( 'Main Menu', 'main'),
-     'social' => __( 'Social Menu', 'social')
+     'social' => __( 'Social Menu', 'social'),
+     'footer' => __( 'Footer Menu', 'footer')
 ));
 
 
